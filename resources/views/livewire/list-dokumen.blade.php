bb21f7f0c562193f1d00b4a3fd0c8a73687497a3
Kamil Raihan <kamilraihanpermana@gmail.com>
2023-10-10 17:28:53 +0700

    commit keempat

--- resources/views/livewire/list-dokumen.blade.php
+++ resources/views/livewire/list-dokumen.blade.php
@@ -3,16 +3,19 @@
         <table class="table table-bordered table-striped">
             <thead>
             <tr>
+              <th><input type="checkbox" name="" id=""></th>
               <th>ID Dokumen</th>
               <th>Jenis Dokumen</th>
               <th>Penandatangan</th>
               <th>Status Dokumen</th>
               <th>Nomor Induk Dokumen</th>
+              <th>Detail</th>
             </tr>
             </thead>
             <tbody>
                 @foreach ($document as $d)
                 <tr>
+                    <td><input type="checkbox" name="" id=""></td>
                     <td>{{ $d->DocumentID }}</td>
                     <td>{{ $d->jenis_dokumen->JenisDokumen }}</td>
                     <td>{{ $d->sign->sign_by }}</td>
@@ -29,6 +32,108 @@
                     @else
                     <td><strong>Nomor Induk Dokumen : </strong> {{ $d->DocumentDescParent }}</td>
                     @endif
+                    <td><button class="btn btn-sm btn-info" data-toggle="modal" data-target="#modal-history">Histori Dokumen</button></td>
+                    <div class="modal fade" id="modal-history">
+                        <div class="modal-dialog modal-lg">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h4 class="modal-title">Histori Dokumen</h4>
+                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="container-fluid">
+                                    <!-- Timelime example  -->
+                                    <div class="row">
+                                      <div class="col-md-12">
+                                        <!-- The time line -->
+                                        <div class="timeline">
+                                          <!-- timeline time label -->
+                                          <div class="time-label">
+                                            <span class="bg-red">27 Sep 2023</span>
+                                          </div>
+                                          <!-- /.timeline-label -->
+                                          <!-- timeline item -->
+                                          <div>
+                                            <i class="fas fa-envelope bg-blue"></i>
+                                            <div class="timeline-item">
+                                              <span class="time"><i class="fas fa-clock"></i> 12:05</span>
+                                              <h3 class="timeline-header"><a href="#">Dokumen diambil Mitra</a></h3>
+
+                                              <div class="timeline-body">
+                                                Dokumen diambil Mitra (PIC: Fulan)
+                                              </div>
+                                            </div>
+                                          </div>
+                                          <!-- END timeline item -->
+                                          <!-- timeline item -->
+                                          <div>
+
+                                          </div>
+                                          <!-- END timeline item -->
+                                          <!-- timeline item -->
+                                          <div>
+                                            <i class="fas fa-comments bg-yellow"></i>
+                                            <div class="timeline-item">
+                                              <span class="time"><i class="fas fa-clock"></i> 27 mins ago</span>
+                                              <h3 class="timeline-header"><a href="#">Dokumen Selesai TTD Manager</a></h3>
+                                              <div class="timeline-body">
+                                                Diupdate oleh M. Ilham Syafrizal
+                                              </div>
+                                            </div>
+                                          </div>
+                                          <!-- END timeline item -->
+                                          <!-- timeline time label -->
+                                          <div class="time-label">
+                                            <span class="bg-green">25 Sep 2023</span>
+                                          </div>
+                                          <!-- /.timeline-label -->
+                                          <!-- timeline item -->
+                                          <div>
+                                            <i class="fa fa-camera bg-purple"></i>
+                                            <div class="timeline-item">
+                                              <span class="time"><i class="fas fa-clock"></i> 2 days ago</span>
+                                              <h3 class="timeline-header"><a href="#">Dokumen Diproses Manager</a></h3>
+                                              <div class="timeline-body">
+                                                Diupdate oleh M.Agung Prakarsa
+                                              </div>
+                                            </div>
+                                          </div>
+                                          <!-- END timeline item -->
+                                          <!-- timeline item -->
+                                          <div>
+                                            <i class="fas fa-video bg-maroon"></i>
+
+                                            <div class="timeline-item">
+                                              <span class="time"><i class="fas fa-clock"></i> 5 days ago</span>
+
+                                              <h3 class="timeline-header"><a href="#">Dokumen disubmit oleh mitra <strong>ADMINISTRASI MEDIKA</strong></a></h3>
+
+                                              <div class="timeline-body">
+
+                                              </div>
+                                            </div>
+                                          </div>
+                                          <!-- END timeline item -->
+                                          <div>
+                                            <i class="fas fa-clock bg-gray"></i>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <!-- /.col -->
+                                    </div>
+                                  </div>
+                            </div>
+                            <div class="modal-footer justify-content-between">
+                              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                          </div>
+                          <!-- /.modal-content -->
+                        </div>
+                        <!-- /.modal-dialog -->
+                      </div>
                 </tr>
                 @endforeach
             </tbody>
